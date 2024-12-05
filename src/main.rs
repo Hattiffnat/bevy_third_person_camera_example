39bf2812ce6200d74b72b61114051795414eb653
Hattiffnat <my@email.com>
2024-12-05 03:48:46 +0300

    redesigned the control scheme

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use bevy::{input::mouse::MouseMotion, prelude::*};
+use core::f32;
+
+use bevy::{input::mouse::MouseMotion, prelude::*, window::CursorGrabMode};
 use settings::UserSettings;
+
+use bevy::render::{
+    render_asset::RenderAssetUsages,
+    render_resource::{Extent3d, TextureDimension, TextureFormat},
+};
 
 mod settings;
 
@@ -14,9 +21,10 @@ struct ThirdPersCameraPivot;
 
 const CUBE_SPAWN: Vec3 = Vec3::new(0.0, 1.5, 0.0);
 
-const WALK_SPEED: f32 = 4.0;
-const MOUSE_SPEED: f32 = 0.001;
-const TURN_SPEED: f32 = 10.0;
+const WALK_SPEED: f32 = 10.0;
+const MOUSE_SPEED: f32 = 0.8;
+const CAMERA_SPEED: f32 = 1.0;
+const CHAR_TURN_SPEED: f32 = 5.0;
 
 pub fn main() {
     App::new()
@@ -26,19 +34,33 @@ pub fn main() {
         .add_systems(Update, camera_pivot_inherit_cube_translation)
         .add_systems(
             Update,
-            (sync_target_with_camera, character_keyboard_mouse_control)
+            (sync_target_with_camera, character_keyboard_control)
                 .chain()
                 .run_if(direction_command_is_pressed),
         )
         // .add_systems(Update, test_cube_rotation.run_if(arrow_key_is_pressed))
-        .add_systems(Update, my_camera_control)
+        .add_systems(Update, mouse_camera_control)
+        .add_systems(
+            Update,
+            keyboard_camera_control.run_if(_camera_key_is_pressed),
+        )
+        .add_systems(Update, toggle_cursor)
         .add_systems(Update, pivot_gizmo)
-        .add_systems(Update, cube_transform_gizmo)
+        .add_systems(Update, character_transform_gizmo)
         .run();
 }
 
-fn arrow_key_is_pressed(keys: Res<ButtonInput<KeyCode>>) -> bool {
-    keys.any_pressed([KeyCode::ArrowLeft, KeyCode::ArrowRight])
+fn _camera_key_is_pressed(
+    keys: Res<ButtonInput<KeyCode>>,
+    user_settings: Res<settings::UserSettings>,
+) -> bool {
+    let keymap = &user_settings.mouse_keyboard_keymap;
+    keys.any_pressed([
+        keymap.camera_up,
+        keymap.camera_down,
+        keymap.camera_left,
+        keymap.camera_right,
+    ])
 }
 
 fn direction_command_is_pressed(
@@ -57,6 +79,7 @@ fn direction_command_is_pressed(
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     settings: Res<UserSettings>,
 ) {
@@ -75,17 +98,23 @@ fn setup_scene(
 
     commands.spawn(floor);
 
+    let debug_material = materials.add(StandardMaterial {
+        base_color_texture: Some(images.add(uv_debug_texture())),
+        ..default()
+    });
+
     let cube = (
         Name::new("MyCube"),
         ThirdPersCameraTarget,
         Mesh3d(meshes.add(Cuboid::from_length(3.0))),
-        MeshMaterial3d(materials.add(Color::linear_rgb(1.0, 0.0, 0.0))),
+        MeshMaterial3d(debug_material),
         Transform::from_translation(CUBE_SPAWN),
     );
 
     let pivot = (
         Name::new("MyCameraPivot"),
         ThirdPersCameraPivot,
+        Visibility::Hidden,
         Transform::default(),
     );
 
@@ -96,7 +125,8 @@ fn setup_scene(
             fov: 60.0_f32.to_radians(),
             ..default()
         }),
-        Transform::from_translation(settings.camera_settings.translation).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_translation(settings.camera_settings.translation)
+            .looking_at(Vec3::ZERO, Vec3::Y),
         ThirdPersonCamera,
     );
 
@@ -116,6 +146,8 @@ fn sync_target_with_camera(
         &Transform,
         (With<ThirdPersCameraPivot>, Without<ThirdPersCameraTarget>),
     >,
+    user_settings: Res<settings::UserSettings>,
+    keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
 ) {
     // println!("turn_cube_out_of_camera");
@@ -126,60 +158,56 @@ fn sync_target_with_camera(
         return;
     };
 
-    let new_direction = pivot_transf.forward();
-    let Ok(new_direction) = Dir3::from_xyz(new_direction.x, 0.0, new_direction.z) else {
-        return;
-    };
+    let keymap = &user_settings.mouse_keyboard_keymap;
 
-    target_transf.rotation = target_transf.rotation.lerp(
-        target_transf.looking_to(new_direction, Dir3::Y).rotation,
-        TURN_SPEED * time.delta_secs(),
-    );
-}
-
-fn character_keyboard_mouse_control(
-    mut cube_transform_q: Query<
-        &mut Transform,
-        (With<ThirdPersCameraTarget>, Without<ThirdPersCameraPivot>),
-    >,
-    pivot_transform_q: Query<
-        &Transform,
-        (With<ThirdPersCameraPivot>, Without<ThirdPersCameraTarget>),
-    >,
-    user_settings: Res<settings::UserSettings>,
-    keys: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-) {
-    let Ok(mut cube_transf) = cube_transform_q.get_single_mut() else {
-        return;
-    };
-
-    let mk_keymap = &user_settings.mouse_keyboard_keymap;
-
-    if let Some(movement_dir) = {
-        if keys.all_pressed([mk_keymap.forward, mk_keymap.right]) {
-            Some(cube_transf.forward().slerp(cube_transf.right(), 0.5))
-        } else if keys.all_pressed([mk_keymap.forward, mk_keymap.left]) {
-            Some(cube_transf.forward().slerp(cube_transf.left(), 0.5))
-        } else if keys.all_pressed([mk_keymap.back, mk_keymap.right]) {
-            Some(cube_transf.back().slerp(cube_transf.right(), 0.5))
-        } else if keys.all_pressed([mk_keymap.back, mk_keymap.left]) {
-            Some(cube_transf.back().slerp(cube_transf.left(), 0.5))
-        } else if keys.pressed(mk_keymap.forward) {
-            Some(cube_transf.forward())
-        } else if keys.pressed(mk_keymap.back) {
-            Some(cube_transf.back())
-        } else if keys.pressed(mk_keymap.left) {
-            Some(cube_transf.left())
-        } else if keys.pressed(mk_keymap.right) {
-            Some(cube_transf.right())
+    // Направление вычисляется из ориентации опорной точки камеры, без учёта оси Y
+    if let Some(new_dir) = {
+        if keys.all_pressed([keymap.forward, keymap.right]) {
+            Some(pivot_transf.forward().slerp(pivot_transf.right(), 0.5))
+        } else if keys.all_pressed([keymap.forward, keymap.left]) {
+            Some(pivot_transf.forward().slerp(pivot_transf.left(), 0.5))
+        } else if keys.all_pressed([keymap.back, keymap.right]) {
+            Some(pivot_transf.back().slerp(pivot_transf.right(), 0.5))
+        } else if keys.all_pressed([keymap.back, keymap.left]) {
+            Some(pivot_transf.back().slerp(pivot_transf.left(), 0.5))
+        } else if keys.pressed(keymap.forward) {
+            Some(pivot_transf.forward())
+        } else if keys.pressed(keymap.back) {
+            Some(pivot_transf.back())
+        } else if keys.pressed(keymap.left) {
+            Some(pivot_transf.left())
+        } else if keys.pressed(keymap.right) {
+            Some(pivot_transf.right())
         } else {
             None
         }
     } {
-        cube_transf.translation +=
-            movement_dir.normalize() * WALK_SPEED * time.delta_secs()
+        // обнуляем ось Y
+        let Ok(movement_dir) = Dir3::new(new_dir.with_y(0.0)) else {
+            return;
+        };
+
+        target_transf.rotation = target_transf.rotation.rotate_towards(
+            target_transf.looking_to(movement_dir, Dir3::Y).rotation,
+            CHAR_TURN_SPEED * time.delta_secs(),
+        );
     }
+}
+
+fn character_keyboard_control(
+    mut target_transform_q: Query<
+        &mut Transform,
+        (With<ThirdPersCameraTarget>, Without<ThirdPersCameraPivot>),
+    >,
+    time: Res<Time>,
+) {
+    let Ok(mut target_transf) = target_transform_q.get_single_mut() else {
+        return;
+    };
+
+    // двигаем персонажа вперёд
+    let forward = target_transf.forward();
+    target_transf.translation += forward * WALK_SPEED * time.delta_secs()
 }
 
 fn camera_pivot_inherit_cube_translation(
@@ -202,8 +230,29 @@ fn camera_pivot_inherit_cube_translation(
     pivot_transf.translation = cube_transf.translation;
 }
 
-fn my_camera_control(
+fn mouse_camera_control(
     mut mouse_motion_reader: EventReader<MouseMotion>,
+    mut pivot_transform_q: Query<
+        &mut Transform,
+        (With<ThirdPersCameraPivot>, Without<Camera>),
+    >,
+    time: Res<Time>,
+) {
+    let Ok(mut pivot_transf) = pivot_transform_q.get_single_mut() else {
+        return;
+    };
+
+    let delta: Vec2 = mouse_motion_reader.read().map(|motion| motion.delta).sum();
+
+    pivot_transf.rotate_y(-delta.x * MOUSE_SPEED * CAMERA_SPEED * time.delta_secs());
+    pivot_transf
+        .rotate_local_x(-delta.y * MOUSE_SPEED * CAMERA_SPEED * time.delta_secs());
+}
+
+fn keyboard_camera_control(
+    keys: Res<ButtonInput<KeyCode>>,
+    user_settings: Res<settings::UserSettings>,
+    time: Res<Time>,
     mut pivot_transform_q: Query<
         &mut Transform,
         (With<ThirdPersCameraPivot>, Without<Camera>),
@@ -213,44 +262,81 @@ fn my_camera_control(
         return;
     };
 
-    let delta: Vec2 = mouse_motion_reader.read().map(|motion| motion.delta).sum();
+    let keymap = &user_settings.mouse_keyboard_keymap;
 
-    pivot_transf.rotate_y(-delta.x * MOUSE_SPEED);
-    pivot_transf.rotate_local_x(-delta.y * MOUSE_SPEED);
+    if keys.pressed(keymap.camera_up) {
+        pivot_transf.rotate_local_x(-CAMERA_SPEED * time.delta_secs());
+    }
+
+    if keys.pressed(keymap.camera_down) {
+        pivot_transf.rotate_local_x(CAMERA_SPEED * time.delta_secs());
+    }
+
+    if keys.pressed(keymap.camera_left) {
+        pivot_transf.rotate_y(-CAMERA_SPEED * time.delta_secs());
+    }
+
+    if keys.pressed(keymap.camera_right) {
+        pivot_transf.rotate_y(CAMERA_SPEED * time.delta_secs());
+    }
 }
 
 fn pivot_gizmo(
     mut gizmos: Gizmos,
     hoop_global_t_q: Query<&GlobalTransform, With<ThirdPersCameraPivot>>,
 ) {
-    let Ok(hoop_global_t) = hoop_global_t_q.get_single() else {
+    let Ok(pivot_global_t) = hoop_global_t_q.get_single() else {
         return;
     };
-    gizmos.axes(*hoop_global_t, 4.0);
+    gizmos.axes(*pivot_global_t, 4.0);
 }
 
-fn cube_transform_gizmo(
+fn character_transform_gizmo(
     mut gizmos: Gizmos,
-    hoop_global_t_q: Query<&GlobalTransform, With<ThirdPersCameraTarget>>,
+    character_global_t_q: Query<&GlobalTransform, With<ThirdPersCameraTarget>>,
 ) {
-    let Ok(hoop_global_t) = hoop_global_t_q.get_single() else {
+    let Ok(character_global_t) = character_global_t_q.get_single() else {
         return;
     };
-    gizmos.axes(*hoop_global_t, 4.0);
+    gizmos.axes(*character_global_t, 4.0);
 }
 
-fn _cube_rotation(
-    mut cube_transf_q: Query<&mut Transform, With<ThirdPersCameraTarget>>,
-    keys: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-) {
-    let Ok(mut cube_transf) = cube_transf_q.get_single_mut() else {
-        return;
-    };
+/// Creates a colorful test pattern
+fn uv_debug_texture() -> Image {
+    const TEXTURE_SIZE: usize = 8;
 
-    if keys.pressed(KeyCode::ArrowLeft) {
-        cube_transf.rotate_local_y(-TURN_SPEED * time.delta_secs());
-    } else if keys.pressed(KeyCode::ArrowRight) {
-        cube_transf.rotate_local_y(TURN_SPEED * time.delta_secs());
-    };
+    let mut palette: [u8; 32] = [
+        255, 102, 159, 255, 255, 159, 102, 255, 236, 255, 102, 255, 121, 255, 102, 255,
+        102, 255, 198, 255, 102, 198, 255, 255, 121, 102, 255, 255, 236, 102, 255, 255,
+    ];
+
+    let mut texture_data = [0; TEXTURE_SIZE * TEXTURE_SIZE * 4];
+    for y in 0..TEXTURE_SIZE {
+        let offset = TEXTURE_SIZE * y * 4;
+        texture_data[offset..(offset + TEXTURE_SIZE * 4)].copy_from_slice(&palette);
+        palette.rotate_right(4);
+    }
+
+    Image::new_fill(
+        Extent3d {
+            width: TEXTURE_SIZE as u32,
+            height: TEXTURE_SIZE as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        &texture_data,
+        TextureFormat::Rgba8UnormSrgb,
+        RenderAssetUsages::RENDER_WORLD,
+    )
 }
+
+fn toggle_cursor(mut window: Single<&mut Window>, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::Space) {
+        window.cursor_options.visible = !window.cursor_options.visible;
+        window.cursor_options.grab_mode = match window.cursor_options.grab_mode {
+            CursorGrabMode::None => CursorGrabMode::Locked,
+            CursorGrabMode::Locked | CursorGrabMode::Confined => CursorGrabMode::None,
+        };
+    }
+}
+
